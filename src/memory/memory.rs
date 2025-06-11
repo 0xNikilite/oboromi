0f1601d72d51ec5988a392bc0057e0e7659bc639
Nicola <109990495+Nikilites@users.noreply.github.com>
2025-06-11 20:30:30 +0200

    init memory

---
+++ src/memory/memory.rs
@@ -0,0 +1,15 @@
+// scr/memory/memory.rs
+
+/// Represents the system's memory interface
+pub struct Memory {
+    // memory implementation will go here
+}
+
+impl Memory {
+    /// Creates a new instance of the memory
+    pub fn new() -> Self {
+        Memory {
+            // initialize memory contents here later 
+        }
+    }
+}
